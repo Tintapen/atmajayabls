4b84b1ded33082994f91c25ec45f670525c5cef2
pomeepo <pomeepo@gmail.com>
2021-05-27 17:02:29 +0700

    Isi Division

--- application/views/division/v_division.php
+++ application/views/division/v_division.php
@@ -5,11 +5,11 @@
 		<div class="container">
 
 			<div class="d-flex justify-content-between align-items-center">
-				<h2>Team</h2>
-				<ol>
+				<h2>Division</h2>
+				<!-- <ol>
 					<li><a href="index.html">Home</a></li>
-					<li>Team</li>
-				</ol>
+					<li>Division</li>
+				</ol> -->
 			</div>
 
 		</div>
@@ -20,8 +20,8 @@
 		<div class="container">
 
 			<div class="section-title" data-aos="fade-up">
-				<h2>Our <strong>Team</strong></h2>
-				<p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+				<h2>Managing <strong>Executive</strong></h2>
+				<!-- <p>.</p> -->
 			</div>
 
 			<div class="row">
@@ -29,17 +29,17 @@
 				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
 					<div class="member" data-aos="fade-up">
 						<div class="member-img">
-							<img src="assets/img/team/team-1.jpg" class="img-fluid" alt="">
+							<img src="assets/img/abls/Albima Rangga Setiyawan.png" class="img-fluid" alt="">
 							<div class="social">
-								<a href=""><i class="icofont-twitter"></i></a>
-								<a href=""><i class="icofont-facebook"></i></a>
+								<!-- <a href=""><i class="icofont-twitter"></i></a> -->
+								<!-- <a href=""><i class="icofont-facebook"></i></a> -->
 								<a href=""><i class="icofont-instagram"></i></a>
 								<a href=""><i class="icofont-linkedin"></i></a>
 							</div>
 						</div>
 						<div class="member-info">
-							<h4>Walter White</h4>
-							<span>Chief Executive Officer</span>
+							<h4>Albima Rangga Setiyawan</h4>
+							<span>Executive Director</span>
 						</div>
 					</div>
 				</div>
@@ -47,17 +47,17 @@
 				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
 					<div class="member" data-aos="fade-up" data-aos-delay="100">
 						<div class="member-img">
-							<img src="assets/img/team/team-2.jpg" class="img-fluid" alt="">
+						<img src="assets/img/abls/Feira Salsabila Nadyaputri.png" class="img-fluid" alt="">
 							<div class="social">
-								<a href=""><i class="icofont-twitter"></i></a>
-								<a href=""><i class="icofont-facebook"></i></a>
+								<!-- <a href=""><i class="icofont-twitter"></i></a>
+								<a href=""><i class="icofont-facebook"></i></a> -->
 								<a href=""><i class="icofont-instagram"></i></a>
 								<a href=""><i class="icofont-linkedin"></i></a>
 							</div>
 						</div>
 						<div class="member-info">
-							<h4>Sarah Jhonson</h4>
-							<span>Product Manager</span>
+							<h4>Feira Salsabila Nadiaputri</h4>
+							<span>Secretary</span>
 						</div>
 					</div>
 				</div>
@@ -65,17 +65,17 @@
 				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
 					<div class="member" data-aos="fade-up" data-aos-delay="200">
 						<div class="member-img">
-							<img src="assets/img/team/team-3.jpg" class="img-fluid" alt="">
+						<img src="assets/img/abls/Hanurita Ayu Widyasti.png" class="img-fluid" alt="">
 							<div class="social">
-								<a href=""><i class="icofont-twitter"></i></a>
-								<a href=""><i class="icofont-facebook"></i></a>
+								<!-- <a href=""><i class="icofont-twitter"></i></a>
+								<a href=""><i class="icofont-facebook"></i></a> -->
 								<a href=""><i class="icofont-instagram"></i></a>
 								<a href=""><i class="icofont-linkedin"></i></a>
 							</div>
 						</div>
 						<div class="member-info">
-							<h4>William Anderson</h4>
-							<span>CTO</span>
+							<h4>Hanurita Ayu Widyasti</h4>
+							<span>Vice Secretary</span>
 						</div>
 					</div>
 				</div>
@@ -83,24 +83,387 @@
 				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
 					<div class="member" data-aos="fade-up" data-aos-delay="300">
 						<div class="member-img">
-							<img src="assets/img/team/team-4.jpg" class="img-fluid" alt="">
+						<img src="assets/img/abls/Matthew Aprianto.png" class="img-fluid" alt="">
 							<div class="social">
-								<a href=""><i class="icofont-twitter"></i></a>
-								<a href=""><i class="icofont-facebook"></i></a>
+								<!-- <a href=""><i class="icofont-twitter"></i></a>
+								<a href=""><i class="icofont-facebook"></i></a> -->
 								<a href=""><i class="icofont-instagram"></i></a>
 								<a href=""><i class="icofont-linkedin"></i></a>
 							</div>
 						</div>
 						<div class="member-info">
-							<h4>Amanda Jepson</h4>
-							<span>Accountant</span>
+							<h4>Matthew Aprianto</h4>
+							<span>Treasurer</span>
 						</div>
 					</div>
 				</div>
 
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+					<div class="member" data-aos="fade-up" data-aos-delay="300">
+						<div class="member-img">
+						<img src="assets/img/abls/Jessica Jill Valentine.png" class="img-fluid" alt="">
+							<div class="social">
+								<!-- <a href=""><i class="icofont-twitter"></i></a>
+								<a href=""><i class="icofont-facebook"></i></a> -->
+								<a href=""><i class="icofont-instagram"></i></a>
+								<a href=""><i class="icofont-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Jessica Jill Valentine</h4>
+							<span>Vice Treasurer</span>
+						</div>
+					</div>
+				</div>
+
+			
+
 			</div>
 
-		</div>
+	
+		<div class="section-title" data-aos="fade-up">
+				<h2>Departement of <strong>Organizations</strong></h2>
+				<!-- <p>.</p> -->
+			</div>
+
+			<div class="row">
+
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+					<div class="member" data-aos="fade-up" data-aos-delay="300">
+						<div class="member-img">
+						<img src="assets/img/abls/Gandhi Dwiky Mohamad.png" class="img-fluid" alt="">
+							<div class="social">
+								<!-- <a href=""><i class="icofont-twitter"></i></a>
+								<a href=""><i class="icofont-facebook"></i></a> -->
+								<a href=""><i class="icofont-instagram"></i></a>
+								<a href=""><i class="icofont-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Gandhi Dwiky Mohamad</h4>
+							<span>Director of Organizations</span>
+						</div>
+					</div>
+				</div>	
+
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+					<div class="member" data-aos="fade-up" data-aos-delay="300">
+						<div class="member-img">
+						<img src="assets/img/abls/Benedictus Richardo Galla.png" class="img-fluid" alt="">
+							<div class="social">
+								<!-- <a href=""><i class="icofont-twitter"></i></a>
+								<a href=""><i class="icofont-facebook"></i></a> -->
+								<a href=""><i class="icofont-instagram"></i></a>
+								<a href=""><i class="icofont-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Benedictus Richardo Galla</h4>
+							<span>Manager of Internal</span>
+						</div>
+					</div>
+				</div>	
+
+				
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+					<div class="member" data-aos="fade-up" data-aos-delay="300">
+						<div class="member-img">
+						<img src="assets/img/abls/Jonathan Sastyoviandi Lebe.png" class="img-fluid" alt="">
+							<div class="social">
+								<!-- <a href=""><i class="icofont-twitter"></i></a>
+								<a href=""><i class="icofont-facebook"></i></a> -->
+								<a href=""><i class="icofont-instagram"></i></a>
+								<a href=""><i class="icofont-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Jonathan Sastyoviandi Lebe</h4>
+							<span>Manager of External</span>
+						</div>
+					</div>
+				</div>	
+
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+					<div class="member" data-aos="fade-up" data-aos-delay="300">
+						<div class="member-img">
+						<img src="assets/img/abls/Meidita Rahmawati.png" class="img-fluid" alt="">
+							<div class="social">
+								<!-- <a href=""><i class="icofont-twitter"></i></a>
+								<a href=""><i class="icofont-facebook"></i></a> -->
+								<a href=""><i class="icofont-instagram"></i></a>
+								<a href=""><i class="icofont-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Meidita Rahmawati</h4>
+							<span>Manager of Media & Public Relations</span>
+						</div>
+					</div>
+				</div>	
+
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+					<div class="member" data-aos="fade-up" data-aos-delay="300">
+						<div class="member-img">
+						<img src="assets/img/abls/Patricia Inggrid Dewani.png" class="img-fluid" alt="">
+							<div class="social">
+								<!-- <a href=""><i class="icofont-twitter"></i></a>
+								<a href=""><i class="icofont-facebook"></i></a> -->
+								<a href=""><i class="icofont-instagram"></i></a>
+								<a href=""><i class="icofont-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Patricia Inggrid Dewani</h4>
+							<span>Staff of Internal</span>
+						</div>
+					</div>
+				</div>	
+
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+					<div class="member" data-aos="fade-up" data-aos-delay="300">
+						<div class="member-img">
+						<img src="assets/img/abls/Jessica Evangeli Santoso.png" class="img-fluid" alt="">
+							<div class="social">
+								<!-- <a href=""><i class="icofont-twitter"></i></a>
+								<a href=""><i class="icofont-facebook"></i></a> -->
+								<a href=""><i class="icofont-instagram"></i></a>
+								<a href=""><i class="icofont-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Jessica Evangeli Santoso</h4>
+							<span>Staff of Internal</span>
+						</div>
+					</div>
+				</div>	
+
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+					<div class="member" data-aos="fade-up" data-aos-delay="300">
+						<div class="member-img">
+						<img src="assets/img/abls/Fillipe Larry.png" class="img-fluid" alt="">
+							<div class="social">
+								<!-- <a href=""><i class="icofont-twitter"></i></a>
+								<a href=""><i class="icofont-facebook"></i></a> -->
+								<a href=""><i class="icofont-instagram"></i></a>
+								<a href=""><i class="icofont-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Fillipe Larry</h4>
+							<span>Staff of External</span>
+						</div>
+					</div>
+				</div>	
+
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+					<div class="member" data-aos="fade-up" data-aos-delay="300">
+						<div class="member-img">
+						<img src="assets/img/abls/Athaya Dyandra Alikashari.png" class="img-fluid" alt="">
+							<div class="social">
+								<!-- <a href=""><i class="icofont-twitter"></i></a>
+								<a href=""><i class="icofont-facebook"></i></a> -->
+								<a href=""><i class="icofont-instagram"></i></a>
+								<a href=""><i class="icofont-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Athaya Dyandra Alikashari</h4>
+							<span>Staff of External</span>
+						</div>
+					</div>
+				</div>	
+
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+					<div class="member" data-aos="fade-up" data-aos-delay="300">
+						<div class="member-img">
+						<img src="assets/img/abls/Naufal Hisyam.png" class="img-fluid" alt="">
+							<div class="social">
+								<!-- <a href=""><i class="icofont-twitter"></i></a>
+								<a href=""><i class="icofont-facebook"></i></a> -->
+								<a href=""><i class="icofont-instagram"></i></a>
+								<a href=""><i class="icofont-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Naufal Hisyam</h4>
+							<span>Staff of External</span>
+						</div>
+					</div>
+				</div>	
+
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+					<div class="member" data-aos="fade-up" data-aos-delay="300">
+						<div class="member-img">
+						<img src="assets/img/abls/Anastasya Pambajeng.png" class="img-fluid" alt="">
+							<div class="social">
+								<!-- <a href=""><i class="icofont-twitter"></i></a>
+								<a href=""><i class="icofont-facebook"></i></a> -->
+								<a href=""><i class="icofont-instagram"></i></a>
+								<a href=""><i class="icofont-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Anastasya Pambajeng D.R</h4>
+							<span>Staff of Media & PR</span>
+						</div>
+					</div>
+				</div>	
+
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+					<div class="member" data-aos="fade-up" data-aos-delay="300">
+						<div class="member-img">
+						<img src="assets/img/abls/Gabriella Jasmine.png" class="img-fluid" alt="">
+							<div class="social">
+								<!-- <a href=""><i class="icofont-twitter"></i></a>
+								<a href=""><i class="icofont-facebook"></i></a> -->
+								<a href=""><i class="icofont-instagram"></i></a>
+								<a href=""><i class="icofont-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Gabriella Jasmine M</h4>
+							<span>Staff of Media & PR</span>
+						</div>
+					</div>
+				</div>	
+
+				<!-- <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+					<div class="member" data-aos="fade-up" data-aos-delay="300">
+						<div class="member-img">
+						<img src="assets/img/abls/Adam Prameza Anggito.png" class="img-fluid" alt="">
+							<div class="social">
+								<a href=""><i class="icofont-instagram"></i></a>
+								<a href=""><i class="icofont-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Adam Prameza Anggito</h4>
+							<span>Staff of Media & PR</span>
+						</div>
+					</div>
+				</div>	 -->
+
+			</div>	
+		
+			<div class="section-title" data-aos="fade-up">
+				<h2>Departement of <strong>Research</strong></h2>
+				<!-- <p>.</p> -->
+			</div>
+
+			<div class="row">
+
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+					<div class="member" data-aos="fade-up" data-aos-delay="300">
+						<div class="member-img">
+						<img src="assets/img/abls/James Xaverius Lemi.png" class="img-fluid" alt="">
+							<div class="social">
+								<!-- <a href=""><i class="icofont-twitter"></i></a>
+								<a href=""><i class="icofont-facebook"></i></a> -->
+								<a href=""><i class="icofont-instagram"></i></a>
+								<a href=""><i class="icofont-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>James Xaverius Lemi</h4>
+							<span>Director of Research</span>
+						</div>
+					</div>
+				</div>	
+
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+					<div class="member" data-aos="fade-up" data-aos-delay="300">
+						<div class="member-img">
+						<img src="assets/img/abls/Immanuel Owen.png" class="img-fluid" alt="">
+							<div class="social">
+								<!-- <a href=""><i class="icofont-twitter"></i></a>
+								<a href=""><i class="icofont-facebook"></i></a> -->
+								<a href=""><i class="icofont-instagram"></i></a>
+								<a href=""><i class="icofont-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Immanuel Owen</h4>
+							<span>Capital Market Division</span>
+						</div>
+					</div>
+				</div>	
+
+				
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+					<div class="member" data-aos="fade-up" data-aos-delay="300">
+						<div class="member-img">
+						<img src="assets/img/abls/Ryan Ricardo.png" class="img-fluid" alt="">
+							<div class="social">
+								<!-- <a href=""><i class="icofont-twitter"></i></a>
+								<a href=""><i class="icofont-facebook"></i></a> -->
+								<a href=""><i class="icofont-instagram"></i></a>
+								<a href=""><i class="icofont-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Ryan Ricardo</h4>
+							<span>Banking &Finance Division</span>
+						</div>
+					</div>
+				</div>	
+
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+					<div class="member" data-aos="fade-up" data-aos-delay="300">
+						<div class="member-img">
+						<img src="assets/img/abls/Darren Putranto.png" class="img-fluid" alt="">
+							<div class="social">
+								<!-- <a href=""><i class="icofont-twitter"></i></a>
+								<a href=""><i class="icofont-facebook"></i></a> -->
+								<a href=""><i class="icofont-instagram"></i></a>
+								<a href=""><i class="icofont-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Darren Putranto</h4>
+							<span>Corporate Law Division</span>
+						</div>
+					</div>
+				</div>	
+
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+					<div class="member" data-aos="fade-up" data-aos-delay="300">
+						<div class="member-img">
+						<img src="assets/img/abls/Alexius Krisnantyo.png" class="img-fluid" alt="">
+							<div class="social">
+								<!-- <a href=""><i class="icofont-twitter"></i></a>
+								<a href=""><i class="icofont-facebook"></i></a> -->
+								<a href=""><i class="icofont-instagram"></i></a>
+								<a href=""><i class="icofont-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Alexius Krisnantyo</h4>
+							<span>Energy Mineral Division</span>
+						</div>
+					</div>
+				</div>	
+
+				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+					<div class="member" data-aos="fade-up" data-aos-delay="300">
+						<div class="member-img">
+						<img src="assets/img/abls/Michael Adrian Young.png" class="img-fluid" alt="">
+							<div class="social">
+								<!-- <a href=""><i class="icofont-twitter"></i></a>
+								<a href=""><i class="icofont-facebook"></i></a> -->
+								<a href=""><i class="icofont-instagram"></i></a>
+								<a href=""><i class="icofont-linkedin"></i></a>
+							</div>
+						</div>
+						<div class="member-info">
+							<h4>Michael Adrian Young</h4>
+							<span>Intelectual Property</span>
+						</div>
+					</div>
+				</div>	
+
+				</div>
+				</div>
+			
 	</section><!-- End Our Team Section -->
 
 </main><!-- End #main -->
