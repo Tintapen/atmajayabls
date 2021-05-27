44cd2612cb8a92ddfe83f914d1967f3f38650a07
Tintapen <okiper12@gmail.com>
2021-05-27 15:53:36 +0700

    perubahan konten division

--- application/views/_partials/header.php
+++ application/views/_partials/header.php
@@ -1,20 +1,20 @@
 <div class="container d-flex align-items-center">
 
 	<!-- <img src="assets/img/abls/ABLC.png" class="img-fluid" alt=""> -->
-	<a href="<?= base_url() ?>"><img src="assets/img/abls/2.png" class="img-fluid" style="width:65px;height:70px;">
-	<h1 class="logo mr-auto"><a href="<?= base_url() ?>"><span>AB</span>LS</a></h1>
-	<!-- Uncomment below if you prefer to use an image logo -->
-	<!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/abls/2.png" class="img-fluid"></a> -->
+	<a href="<?= base_url() ?>" class="logo mr-1"><img src="assets/img/abls/2.png" class="img-fluid">
+		<h1 class="logo mr-auto"><a href="<?= base_url() ?>"><span>AB</span>LS</a></h1>
+		<!-- Uncomment below if you prefer to use an image logo -->
+		<!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/abls/2.png" class="img-fluid"></a> -->
 
-	<nav class="nav-menu d-none d-lg-block">
-		<ul>
-			<li class="active"><a href="<?= base_url() ?>">Home</a></li>
+		<nav class="nav-menu d-none d-lg-block">
+			<ul>
+				<li <?php if ($this->uri->segment(1) == '') echo "class='active'"; ?>><a href="<?= base_url() ?>">Home</a></li>
 
-			<li class="drop-down"><a href="">About</a>
-				<ul>
-					<li><a href="<?= base_url('aboutus') ?>">About Us</a></li>
-					<li><a href="<?= base_url('contact') ?>">Contact Us</a></li>
-					<!-- <li><a href="testimonials.html">Testimonials</a></li>
+				<li class="drop-down"><a href="">About</a>
+					<ul>
+						<li><a href="<?= base_url('aboutus') ?>">About Us</a></li>
+						<li><a href="<?= base_url('contact') ?>">Contact Us</a></li>
+						<!-- <li><a href="testimonials.html">Testimonials</a></li>
 					<li class="drop-down"><a href="#">Deep Drop Down</a>
 						<ul>
 							<li><a href="#">Deep Drop Down 1</a></li>
@@ -24,22 +24,22 @@
 							<li><a href="#">Deep Drop Down 5</a></li>
 						</ul>
 					</li> -->
-				</ul>
-			</li>
+					</ul>
+				</li>
 
-			<li><a href="<?= base_url('Division') ?>">Divisions</a></li>
-			<li><a href="<?= base_url('achievement') ?>">Achievement</a></li>
-			<li><a href="<?= base_url('Provent') ?>">Project & Events</a></li>
-			<li><a href="<?= base_url('legal') ?>">ABLS Legal Review</a></li>
+				<li <?php if ($this->uri->segment(1) == 'division') echo "class='active'"; ?>><a href="<?= base_url('division') ?>">Divisions</a></li>
+				<li <?php if ($this->uri->segment(1) == 'achievement') echo "class='active'"; ?>><a href="<?= base_url('achievement') ?>">Achievement</a></li>
+				<li <?php if ($this->uri->segment(1) == 'provent') echo "class='active'"; ?>><a href="<?= base_url('provent') ?>">Project & Events</a></li>
+				<li <?php if ($this->uri->segment(1) == 'legal') echo "class='active'"; ?>><a href="<?= base_url('legal') ?>">ABLS Legal Review</a></li>
 
-		</ul>
-	</nav>
+			</ul>
+		</nav>
 
-	<div class="header-social-links">
-		<!-- <a href="#" class="twitter"><i class="icofont-twitter"></i></a> -->
-		<!-- <a href="#" class="facebook"><i class="icofont-facebook"></i></a> -->
-		<a href="https://www.instagram.com/atmajayabls" class="instagram"><i class="icofont-instagram"></i></a>
-		<a href="#" class="linkedin"><i class="icofont-linkedin"></i></i></a>
-	</div>
+		<div class="header-social-links">
+			<!-- <a href="#" class="twitter"><i class="icofont-twitter"></i></a> -->
+			<!-- <a href="#" class="facebook"><i class="icofont-facebook"></i></a> -->
+			<a href="https://www.instagram.com/atmajayabls" class="instagram"><i class="icofont-instagram"></i></a>
+			<a href="#" class="linkedin"><i class="icofont-linkedin"></i></i></a>
+		</div>
 
 </div>
