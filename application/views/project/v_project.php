aa69ee7ff22707f05b128a20dc555e87e32ad166
Tintapen <okiper12@gmail.com>
2021-06-04 13:44:56 +0700

    paragraph project&event

--- application/views/project/v_project.php
+++ application/views/project/v_project.php
@@ -1,108 +1,43 @@
 <main id="main">
 
-    <!-- ======= Breadcrumbs ======= -->
-    <section id="breadcrumbs" class="breadcrumbs">
-      <div class="container">
+	<section id="breadcrumbs" class="breadcrumbs">
+		<div class="container">
 
-        <div class="d-flex justify-content-between align-items-center">
-          <h2>Project & Events</h2>
-          <!-- <ol>
-            <li><a href="index.html">Home</a></li>
-            <li>Portfolio Details</li>
-          </ol> -->
-        </div>
+			<div class="d-flex justify-content-between align-items-center">
+				<h2>Project & Events</h2>
+			</div>
 
-      </div>
-    </section><!-- End Breadcrumbs -->
+		</div>
+	</section>
 
-    <!-- ======= Portfolio Details Section ======= -->
-    <section id="portfolio-details" class="portfolio-details">
-      <div class="container" data-aos="fade-up">
-     
+	<section id="portfolio-details" class="portfolio-details">
+		<div class="container" data-aos="fade-up">
 
-      <div class="section-title mt-5" data-aos="fade-up">
+
+			<div class="section-title mt-5" data-aos="fade-up">
 				<h2>Project & <strong>Events</strong></h2>
 			</div>
-        <!-- <div class="portfolio-details-container"> -->
-        <div class="row d-flex justify-content-center">
-        <a href="nblc"><img src="assets/img/abls/Logo NBLC Horizontal 2.png" class="img-fluid" alt="" style="position:relative; width:250px;height:140px;"></a>
-        <a href="cla">   <img src="assets/img/abls/Photo-1.png" class="img-fluid" alt="" style="position:relative; width:260px;height:165px;"></a>
-        <a href="iloc">  <img src="assets/img/abls/3.png" class="img-fluid" alt="" style="position:relative; width:150px;height:135px;"></a>
-        <a href="ablc">  <img src="assets/img/abls/ABLC.png" class="img-fluid" alt="" style="position:relative; width:150px;height:150px;"></a>
-        </div>
 
-     
-        <!-- <div class="portfolio-description">
-          <h2>Corporate Legal Academy</h2>
-          <p>
-          CLA is a business law-based webinar event that will explain the aspects of
-            business lawn presented by a speaker <br>from Atma Jaya’s law faculty Alumni who
-            has experience in a law field. In CLA, you can expand your knowledge about <br>
-            business law, you’ll get insights from the top speakers who has a lot of
-            experience in law field, and you can relearn <br> the knowledge  by re-watching the
-            material that we give.
-          </p>
-        </div>
+			<div class="row d-flex justify-content-center">
+				<a href="nblc">
+					<img src="assets/img/abls/Logo NBLC Horizontal 2.png" class="img-fluid" alt="" style="position:relative; width:250px;height:140px;">
+					<p class="text-center">About NBLC Summit</p>
+				</a>
+				<a href="cla"><img src="assets/img/abls/Photo-1.png" class="img-fluid" alt="" style="position:relative; width:260px;height:165px;"></a>
+				<a href="iloc"><img src="assets/img/abls/3.png" class="img-fluid" alt="" style="position:relative; width:150px;height:135px;"></a>
+				<a href="ablc"><img src="assets/img/abls/ABLC.png" class="img-fluid" alt="" style="position:relative; width:150px;height:150px;"></a>
+			</div>
+			<!-- <div class="testimonial-item">
+				<img src="assets/img/abls/Logo NBLC Horizontal 2.png" class="testimonial-img" alt="" style="position:relative; width:250px;height:140px;">
+				<h3>Saul Goodman</h3>
+				<h4>Ceo &amp; Founder</h4>
+				<p>
+					<i class="bx bxs-quote-alt-left quote-icon-left"></i>
+					Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+					<i class="bx bxs-quote-alt-right quote-icon-right"></i>
+				</p>
+			</div> -->
+		</div>
+	</section>
 
-      </div>
-      <div class="container" data-aos="fade-up">
-        <div class="portfolio-details-container">
-
-        <img src="assets/img/abls/Logo NBLC Horizontal 2.png" class="img-fluid" alt="" style="position:relative; width:350px;height:200px;">
-
-        </div>
-
-        <div class="portfolio-description">
-          <h2>NBLC Summit</h2>
-          <p>
-          NBLC is committed organization consist of various Universities located in
-            Indonesia which specialises in Business Law. It has mainstay programs including
-            NBLC Summit which will be held in February 2021 at Atma Jaya Chatolic
-            niversity of Indonesia. The theme of this period’s Summit is “Enhancing the
-            knowledge in the field of Banking Law in Indonesia in order to Mitigate the
-            Impact of COVID-19”.
-          </p>
-        </div>
-
-      </div>
-      <div class="container" data-aos="fade-up">
-        <div class="portfolio-details-container">
-
-       
-          <img src="assets/img/abls/ABLC.png" class="img-fluid" alt="" style="position:relative; width:300px;height:300px;">
-        
-
-        </div>
-
-        <div class="portfolio-description">
-          <h2>Atma Jaya Business Law Competition</h2>
-          <p>
-          ABLC is a Business law competition that aims to develop law students of
-            Indonesia by Atma Jaya Business Law society.<br> ABLC also aims to be forum for
-        all faculty students law throughout Indonesia to hone insights about business law.
-          </p>
-        </div>
-
-      </div>
-      <div class="container" data-aos="fade-up">
-        <div class="portfolio-details-container">
-
-       
-          <img src="assets/img/abls/3.png" class="img-fluid" alt="" style="position:relative; width:300px;height:280px;">
-    
-        </div>
-
-        <div class="portfolio-description">
-          <h2>Atma Jaya Internal Legal Opinion Competition</h2>
-          <p>
-          Atma Jaya ILOC is a legal opinion competition for Atma Jaya’s active law
-        students. It’s one of ABLS’ new programs which <br>will be held in April 2021. The
-        goal is to sharpen and develop the quality of our active students’ skills in
-        expressing their <br> opinions on a complex legal case.
-          </p>
-        </div> -->
-
-      </div>
-    </section><!-- End Portfolio Details Section -->
-
-  </main>
+</main>
