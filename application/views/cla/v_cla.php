66fc90dd32e18eb16e5e9061fe6e302212cd143f
Tintapen <okiper12@gmail.com>
2021-06-14 10:48:55 +0700

    penambahan css dan content carousel pembicara, perubahan style hero content img

--- application/views/cla/v_cla.php
+++ application/views/cla/v_cla.php
@@ -1,56 +1,103 @@
-<section id="hero">
-	<div class="hero-image">
-		<div class="carousel-item active" style="background-image: url(assets/img/abls/cla/panitia.jpg);">
-			<div class="hero-text">
-				<h1>Corporate Legal Academy</h1>
-			</div>
-		</div>
-	</div>
-</section>
+ <section id="hero">
+ 	<div class="hero-image">
+ 		<div class="carousel-item active" style="background-image: url(assets/img/abls/cla/panitia.jpg);">
+ 			<div class="hero-text">
+ 				<h1>Corporate Legal Academy</h1>
+ 			</div>
+ 		</div>
+ 	</div>
+ </section>
 
-<main id="main">
-	<section id="about-us" class="about-us">
-		<div class="container" data-aos="fade-up">
-		<div class="section-title mt-5" data-aos="fade-up">
-				<h2><strong>Corporate Legal Academy</strong></h2>
-			</div>
-			<div class="row content">
-				<div class="col-lg-6 d-flex align-items-center justify-content-center" data-aos="fade-right">
-				<img src="assets/img/abls/Photo-1.png" class="img-fluid" alt="Corporate Legal Academy">
+ <main id="main">
+ 	<section id="about-us" class="about-us">
+ 		<div class="container" data-aos="fade-up">
+ 			<div class="section-title mt-5" data-aos="fade-up">
+ 				<h2><strong>Corporate Legal Academy</strong></h2>
+ 			</div>
+ 			<div class="row content">
+ 				<div class="col-lg-6 d-flex align-items-center justify-content-center" data-aos="fade-right">
+ 					<img src="assets/img/abls/Photo-1.png" class="img-fluid" alt="Corporate Legal Academy">
 
-				</div>
-				<div class="col-lg-6 pt-4 pt-lg-0" data-aos="fade-left">
-					<p>
-						Corporate Legal Academy (“CLA”) was found in 2020 by the Board of Management
-						Atma Jaya Business Law Society (“ABLS”)2019/2020 in order to strengthen our
-						contribution in the field of law academic by conducting a variety of events
-						that have a strong quality yet influences law student to achieve their goals.
-					</p>
+ 				</div>
+ 				<div class="col-lg-6 pt-4 pt-lg-0" data-aos="fade-left">
+ 					<p>
+ 						Corporate Legal Academy (“CLA”) was found in 2020 by the Board of Management
+ 						Atma Jaya Business Law Society (“ABLS”)2019/2020 in order to strengthen our
+ 						contribution in the field of law academic by conducting a variety of events
+ 						that have a strong quality yet influences law student to achieve their goals.
+ 					</p>
 
-					<p>
-						CLA is a series of webinar classes featuring alumni of Atma Jaya Faculty of
-						Law across generation as speakers of this series. CLA aims to equip both Atma Jaya
-						students and external students with the fundamental knowledge of business law along
-						with its practical implementation. By joining CLA, you will get insights from the
-						top speakers who are distinguished business law expertise.
-					</p>
-					<p>
-						This is a non-profit event, whereby any attendance fee from external will be donated
-						as charity for people who are affected during the Covid-19.</p>
-				</div>
-			</div>
-		</div>
-	</section>
+ 					<p>
+ 						CLA is a series of webinar classes featuring alumni of Atma Jaya Faculty of
+ 						Law across generation as speakers of this series. CLA aims to equip both Atma Jaya
+ 						students and external students with the fundamental knowledge of business law along
+ 						with its practical implementation. By joining CLA, you will get insights from the
+ 						top speakers who are distinguished business law expertise.
+ 					</p>
+ 					<p>
+ 						This is a non-profit event, whereby any attendance fee from external will be donated
+ 						as charity for people who are affected during the Covid-19.</p>
+ 				</div>
+ 			</div>
+ 		</div>
+ 	</section>
 
-	<section id="team" class="team section-bg">
-		<div class="container">
+ 	<section id="team" class="team section-bg">
+ 		<div class="container">
 
-			<div class="section-title mt-5" data-aos="fade-up">
-				<h2>Speaker<strong> List</strong></h2>
-			</div>
+ 			<div class="section-title mt-5" data-aos="fade-up">
+ 				<h2>Speaker<strong> List</strong></h2>
+ 			</div>
 
+ 			<div id="speaker" class="row carousel slide" data-ride="carousel">
+ 				<ul class="carousel-indicators">
+ 					<li data-target="#speaker" data-slide-to="0" class="active"></li>
+ 					<li data-target="#speaker" data-slide-to="1"></li>
+ 					<li data-target="#speaker" data-slide-to="2"></li>
+ 				</ul>
 
-			<div class="row d-flex justify-content-center">
+ 				<div class="container carousel-inner p-0">
+ 					<div class="carousel-item active">
+ 						<div class="col-xs-3 col-sm-3 col-md-3">
+ 							<img src="<?= base_url('assets/img/abls/cla/Praniti.jpg') ?>" class="img-fluid">
+ 						</div>
+ 						<div class="col-xs-3 col-sm-3 col-md-3">
+ 							<img src="<?= base_url('assets/img/abls/cla/Ivor.jpg') ?>" class="img-fluid">
+ 						</div>
+ 						<div class="col-xs-3 col-sm-3 col-md-3">
+ 							<img src="<?= base_url('assets/img/abls/cla/GS-BW-002.jpg') ?>" class="img-fluid">
+ 						</div>
+ 						<div class="col-xs-3 col-sm-3 col-md-3">
+ 							<img src="<?= base_url('assets/img/abls/cla/GS-BW-006.jpg') ?>" class="img-fluid">
+ 						</div>
+ 					</div>
+ 					<div class="carousel-item">
+ 						<div class="col-xs-3 col-sm-3 col-md-3">
+ 							<img src="<?= base_url('assets/img/abls/cla/Praniti.jpg') ?>" class="img-fluid">
+ 						</div>
+ 						<div class="col-xs-3 col-sm-3 col-md-3">
+ 							<img src="<?= base_url('assets/img/abls/cla/Ivor.jpg') ?>" class="img-fluid">
+ 						</div>
+ 						<div class="col-xs-3 col-sm-3 col-md-3">
+ 							<img src="<?= base_url('assets/img/abls/cla/GS-BW-002.jpg') ?>" class="img-fluid">
+ 						</div>
+ 						<div class="col-xs-3 col-sm-3 col-md-3">
+ 							<img src="<?= base_url('assets/img/abls/cla/GS-BW-006.jpg') ?>" class="img-fluid">
+ 						</div>
+ 					</div>
+ 				</div>
+
+ 				<a class="carousel-control-prev" href="#speaker" role="button" data-slide="prev">
+ 					<span class="carousel-control-prev-icon icofont-simple-left" aria-hidden="true"></span>
+ 					<span class="sr-only">Previous</span>
+ 				</a>
+ 				<a class="carousel-control-next" href="#speaker" role="button" data-slide="next">
+ 					<span class="carousel-control-next-icon icofont-simple-right" aria-hidden="true"></span>
+ 					<span class="sr-only">Next</span>
+ 				</a>
+ 			</div>
+
+ 			<!-- <div class="row d-flex justify-content-center">
 				<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
 					<div class="member" data-aos="fade-up">
 						<div class="member-img">
@@ -243,140 +290,139 @@
 						</div>
 					</div>
 				</div>
-			</div>
-	</section>
+			</div> -->
+ 		</div>
+ 	</section>
 
-	<section id="session" class="blog-area pt-170 pb-140">
-		<div class="container" data-aos="fade-up">
-			<div class="section-title mt-5" data-aos="fade-up">
-				<h2><strong>This Year's Topics</strong></h2>
-			</div>
+ 	<section id="session" class="blog-area pt-170 pb-140">
+ 		<div class="container" data-aos="fade-up">
+ 			<div class="section-title mt-5" data-aos="fade-up">
+ 				<h2><strong>This Year's Topics</strong></h2>
+ 			</div>
 
-			<div class="row portfolio-description">
-				<div class="col-5 col-sm-3">
-					<div class="nav flex-column nav-tabs h-100" id="vert-tabs-tab" role="tablist" aria-orientation="vertical">
-						<a class="nav-link active" id="vert-tabs-home-tab" data-toggle="pill" href="#vert-tabs-home" role="tab" aria-controls="vert-tabs-home" aria-selected="true">Day 1: Sat,13 March 2021</a>
-						<a class="nav-link" id="vert-tabs-profile-tab" data-toggle="pill" href="#vert-tabs-profile" role="tab" aria-controls="vert-tabs-profile" aria-selected="false">Day 2: Sat,20 March 2021 </a>
-						<a class="nav-link" id="vert-tabs-messages-tab" data-toggle="pill" href="#vert-tabs-messages" role="tab" aria-controls="vert-tabs-messages" aria-selected="false">Day 3: Sat,27 March 2021</a>
-						<a class="nav-link" id="vert-tabs-settings-tab" data-toggle="pill" href="#vert-tabs-settings" role="tab" aria-controls="vert-tabs-settings" aria-selected="false">Day 4: Sat,3 April 2021</a>
-						<a class="nav-link" id="vert-tabs-settings1-tab" data-toggle="pill" href="#vert-tabs-settings1" role="tab" aria-controls="vert-tabs-settings" aria-selected="false">Day 5: Sat,10 April 2021</a>
-						<a class="nav-link" id="vert-tabs-settings2-tab" data-toggle="pill" href="#vert-tabs-settings2" role="tab" aria-controls="vert-tabs-settings" aria-selected="false">Day 6: Sat,17 April 2021</a>
-						<a class="nav-link" id="vert-tabs-settings3-tab" data-toggle="pill" href="#vert-tabs-settings3" role="tab" aria-controls="vert-tabs-settings" aria-selected="false">Day 7: Sat,24 April 2021</a>
-						<a class="nav-link" id="vert-tabs-settings4-tab" data-toggle="pill" href="#vert-tabs-settings4" role="tab" aria-controls="vert-tabs-settings" aria-selected="false">Day 8: Sat,8 May 2021</a>
-					</div>
-				</div>
-				<div class="col-7 col-sm-9">
-					<div class="tab-content" id="vert-tabs-tabContent">
-						<div class="tab-pane text-left fade show active" id="vert-tabs-home" role="tabpanel" aria-labelledby="vert-tabs-home-tab">
-							<ul>
-							<li>
-							<b>1st Session 14:00-16:00</b>
-							</li>
-							
-							Drafting and Negotiating Commercial Contracts
-							</ul>
-						</div>
-						<div class="tab-pane fade" id="vert-tabs-profile" role="tabpanel" aria-labelledby="vert-tabs-profile-tab">
-						<ul>
-							<li>
-							<b>1st Session 10:00-12:00</b>
-							</li>
-							Commercial Dispute Settlement
-						</ul>
-						<ul>
-							<li>
-							<b>2nd Session 14:00-16:00</b>
-							</li>
-							Problem of Suspension of Debt Payment Obligation<br>
-							and Bankruptcy for State-Owned Enterprises.
-							</ul>
-						</div>
-						<div class="tab-pane fade" id="vert-tabs-messages" role="tabpanel" aria-labelledby="vert-tabs-messages-tab">
-						<ul>
-							<li>
-							<b>1st Session 10:00-12:00</b>
-							</li>
-							Merger & Acquisition for Private Limited Liability Company
-						</ul>
-						<ul>
-							<li>
-							<b>2nd Session 14:00-16:00</b>
-							</li>
-							Foreign Investment Roadmap in Indonesia
-							</ul>
-						</div>
-						<div class="tab-pane fade" id="vert-tabs-settings" role="tabpanel" aria-labelledby="vert-tabs-settings-tab">
-						<ul>
-							<li>
-							<b>1st Session 10:00-12:00</b>
-							</li>
-							Global Security Offering
-						</ul>
-						<ul>
-							<li>
-							<b>2nd Session 14:00-16:00</b>
-							</li>
-							Legal aspect of logistic process in international trade
-							</ul>
-						</div>
-						<div class="tab-pane fade" id="vert-tabs-settings1" role="tabpanel" aria-labelledby="vert-tabs-settings1-tab">
-						<ul>
-							<li>
-							<b>1st Session 10:00-12:00</b>
-							</li>
-							The Role of World Trade Organization (WTO)<br>
-							for the Settlement of Trade Disputes
-						</ul>
-						<ul>
-							<li>
-							<b>2nd Session 14:00-16:00</b>
-							</li>
-							Joint Venture in Indonesia
-							</ul>
-						</div>
-						<div class="tab-pane fade" id="vert-tabs-settings2" role="tabpanel" aria-labelledby="vert-tabs-settings2-tab">
-						<ul>
-							<li>
-							<b>1st Session 10:00-12:00</b>
-							</li>
-							Mining in Indonesia- Legal Overview
-						</ul>
-						</div>
-						<div class="tab-pane fade" id="vert-tabs-settings3" role="tabpanel" aria-labelledby="vert-tabs-settings3-tab">
-						<ul>
-							<li>
-							<b>1st Session 10:00-12:00</b>
-							</li>
-							Legal Aspect in Real Estate Transactions and Development
-						</ul>
-						<ul>
-							<li>
-							<b>2nd Session 14:00-16:00</b>
-							</li>
-							Banking & Finance
-							</ul>
-						</div>
-						<div class="tab-pane fade" id="vert-tabs-settings4" role="tabpanel" aria-labelledby="vert-tabs-settings4-tab">
-						<ul>
-							<li>
-							<b>1st Session 10:00-12:00</b>
-							</li>
-							Expatriate Regulation Overview in Indonesia
-						</ul>
-						<ul>
-							<li>
-							<b>2nd Session 14:00-16:00</b>
-							</li>
-							Legal Aspect of Mandatory Covertible Bond <br>
-							as Restructuring Mechanism in Public Company
-							</ul>
-						</div>
+ 			<div class="row portfolio-description">
+ 				<div class="col-5 col-sm-3">
+ 					<div class="nav flex-column nav-tabs h-100" id="vert-tabs-tab" role="tablist" aria-orientation="vertical">
+ 						<a class="nav-link active" id="vert-tabs-home-tab" data-toggle="pill" href="#vert-tabs-home" role="tab" aria-controls="vert-tabs-home" aria-selected="true">Day 1: Sat,13 March 2021</a>
+ 						<a class="nav-link" id="vert-tabs-profile-tab" data-toggle="pill" href="#vert-tabs-profile" role="tab" aria-controls="vert-tabs-profile" aria-selected="false">Day 2: Sat,20 March 2021 </a>
+ 						<a class="nav-link" id="vert-tabs-messages-tab" data-toggle="pill" href="#vert-tabs-messages" role="tab" aria-controls="vert-tabs-messages" aria-selected="false">Day 3: Sat,27 March 2021</a>
+ 						<a class="nav-link" id="vert-tabs-settings-tab" data-toggle="pill" href="#vert-tabs-settings" role="tab" aria-controls="vert-tabs-settings" aria-selected="false">Day 4: Sat,3 April 2021</a>
+ 						<a class="nav-link" id="vert-tabs-settings1-tab" data-toggle="pill" href="#vert-tabs-settings1" role="tab" aria-controls="vert-tabs-settings" aria-selected="false">Day 5: Sat,10 April 2021</a>
+ 						<a class="nav-link" id="vert-tabs-settings2-tab" data-toggle="pill" href="#vert-tabs-settings2" role="tab" aria-controls="vert-tabs-settings" aria-selected="false">Day 6: Sat,17 April 2021</a>
+ 						<a class="nav-link" id="vert-tabs-settings3-tab" data-toggle="pill" href="#vert-tabs-settings3" role="tab" aria-controls="vert-tabs-settings" aria-selected="false">Day 7: Sat,24 April 2021</a>
+ 						<a class="nav-link" id="vert-tabs-settings4-tab" data-toggle="pill" href="#vert-tabs-settings4" role="tab" aria-controls="vert-tabs-settings" aria-selected="false">Day 8: Sat,8 May 2021</a>
+ 					</div>
+ 				</div>
+ 				<div class="col-7 col-sm-9">
+ 					<div class="tab-content" id="vert-tabs-tabContent">
+ 						<div class="tab-pane text-left fade show active" id="vert-tabs-home" role="tabpanel" aria-labelledby="vert-tabs-home-tab">
+ 							<ul>
+ 								<li>
+ 									<b>1st Session 14:00-16:00</b>
+ 								</li>
 
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+ 								Drafting and Negotiating Commercial Contracts
+ 							</ul>
+ 						</div>
+ 						<div class="tab-pane fade" id="vert-tabs-profile" role="tabpanel" aria-labelledby="vert-tabs-profile-tab">
+ 							<ul>
+ 								<li>
+ 									<b>1st Session 10:00-12:00</b>
+ 								</li>
+ 								Commercial Dispute Settlement
+ 							</ul>
+ 							<ul>
+ 								<li>
+ 									<b>2nd Session 14:00-16:00</b>
+ 								</li>
+ 								Problem of Suspension of Debt Payment Obligation<br>
+ 								and Bankruptcy for State-Owned Enterprises.
+ 							</ul>
+ 						</div>
+ 						<div class="tab-pane fade" id="vert-tabs-messages" role="tabpanel" aria-labelledby="vert-tabs-messages-tab">
+ 							<ul>
+ 								<li>
+ 									<b>1st Session 10:00-12:00</b>
+ 								</li>
+ 								Merger & Acquisition for Private Limited Liability Company
+ 							</ul>
+ 							<ul>
+ 								<li>
+ 									<b>2nd Session 14:00-16:00</b>
+ 								</li>
+ 								Foreign Investment Roadmap in Indonesia
+ 							</ul>
+ 						</div>
+ 						<div class="tab-pane fade" id="vert-tabs-settings" role="tabpanel" aria-labelledby="vert-tabs-settings-tab">
+ 							<ul>
+ 								<li>
+ 									<b>1st Session 10:00-12:00</b>
+ 								</li>
+ 								Global Security Offering
+ 							</ul>
+ 							<ul>
+ 								<li>
+ 									<b>2nd Session 14:00-16:00</b>
+ 								</li>
+ 								Legal aspect of logistic process in international trade
+ 							</ul>
+ 						</div>
+ 						<div class="tab-pane fade" id="vert-tabs-settings1" role="tabpanel" aria-labelledby="vert-tabs-settings1-tab">
+ 							<ul>
+ 								<li>
+ 									<b>1st Session 10:00-12:00</b>
+ 								</li>
+ 								The Role of World Trade Organization (WTO)<br>
+ 								for the Settlement of Trade Disputes
+ 							</ul>
+ 							<ul>
+ 								<li>
+ 									<b>2nd Session 14:00-16:00</b>
+ 								</li>
+ 								Joint Venture in Indonesia
+ 							</ul>
+ 						</div>
+ 						<div class="tab-pane fade" id="vert-tabs-settings2" role="tabpanel" aria-labelledby="vert-tabs-settings2-tab">
+ 							<ul>
+ 								<li>
+ 									<b>1st Session 10:00-12:00</b>
+ 								</li>
+ 								Mining in Indonesia- Legal Overview
+ 							</ul>
+ 						</div>
+ 						<div class="tab-pane fade" id="vert-tabs-settings3" role="tabpanel" aria-labelledby="vert-tabs-settings3-tab">
+ 							<ul>
+ 								<li>
+ 									<b>1st Session 10:00-12:00</b>
+ 								</li>
+ 								Legal Aspect in Real Estate Transactions and Development
+ 							</ul>
+ 							<ul>
+ 								<li>
+ 									<b>2nd Session 14:00-16:00</b>
+ 								</li>
+ 								Banking & Finance
+ 							</ul>
+ 						</div>
+ 						<div class="tab-pane fade" id="vert-tabs-settings4" role="tabpanel" aria-labelledby="vert-tabs-settings4-tab">
+ 							<ul>
+ 								<li>
+ 									<b>1st Session 10:00-12:00</b>
+ 								</li>
+ 								Expatriate Regulation Overview in Indonesia
+ 							</ul>
+ 							<ul>
+ 								<li>
+ 									<b>2nd Session 14:00-16:00</b>
+ 								</li>
+ 								Legal Aspect of Mandatory Covertible Bond <br>
+ 								as Restructuring Mechanism in Public Company
+ 							</ul>
+ 						</div>
 
-	
-</main>
+ 					</div>
+ 				</div>
+ 			</div>
+ 		</div>
+ 	</section>
+ </main>
