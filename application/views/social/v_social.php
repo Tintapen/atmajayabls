9f55dd6ff1a0b806166cae5f95b3d47b28b48412
pomeepo <pomeepo@gmail.com>
2021-12-07 17:04:52 +0700

    Penambahan Social

---
+++ application/views/social/v_social.php
@@ -0,0 +1,102 @@
+ <section id="hero">
+     <div class="hero-image">
+         <div class="carousel-item active" style="background-image: url(assets/img/abls/homepage.jpg);">
+             <div class="hero-taxt">
+                 <h1>NBLC Social Responsibility Webinar</h1>
+             </div>
+         </div>
+     </div>
+ </section>
+
+ <main id="main">
+     <section id="about-us" class="about-us section-bg">
+         <div class="container" data-aos="fade-up">
+             <div class="section-title mt-5" data-aos="fade-up">
+                 <h2><strong>NBLC Social Responsibility Webinar</strong></h2>
+             </div>
+             <div class="row content">
+                 <div class="col-lg-6 pt-4 pt-lg-0 text-justify" data-aos="fade-right">
+                     <p>
+                         This talkshow was held on Saturday , November 20th 2021 at 13.30 through online platform and
+                         moderated by by Putri Purbasari R.M., S.H., M.H. (Lecturer at Atma Jaya Unika Faculty of Law).
+                         The program is presented by Atma Jaya Busines Law Society from The Faculty of Law Unika Atma
+                         Jaya with the topic of “Innovation and Strategy in Intelectual Property rights protection for
+                         Micro Enterprises”, with prestigious speakers, such as:
+                     </p>
+
+                     <ul>
+                         <li><i class="ri-check-double-line"></i>Raditya Almuricha (Owner of Streetwear Palugada) </li>
+                         <li><i class="ri-check-double-line"></i>Gerda Netty Octavia, S.H., M.H. (JFT MADYA Brand
+                             Examiner)</li>
+                         <li><i class="ri-check-double-line"></i>Riyo Hanggoro Prasetyo, S.H., M.Kn. (Intellectual
+                             Property Lawyer)</li>
+                     </ul>
+                 </div>
+                 <div class="col-lg-6 pt-4 pt-lg-0 text-justify" data-aos="fade-left">
+
+                     <p>
+                         This talkshow aims to be an inspiration and learning experience for people who are interested
+                         build small business as well as educating Indonesian entrepreneur on Innovations and Strategies
+                         in the Protection of Intellectual Property Rights for Micro, Small and Medium Enterprises.
+                         Fortunately, this talkshow ran smoothly and was attended by approximately 80 participants.
+                     </p>
+                 </div>
+             </div>
+         </div>
+     </section>
+
+
+     <section id="portfolio" class="portfolio section-bg">
+         <div class="container">
+             <div class="section-title mt-5" data-aos="fade-up">
+                 <h2><strong>NBLC Social Responsibility Webinar Documentaries</strong></h2>
+             </div>
+             <div class="row portfolio-container" data-aos="fade-up">
+
+                 <div class="col-lg-4 col-md-6 portfolio-item filter">
+                     <a href="assets/img/abls/homepage.jpg" data-gall="portfolioGallery" class="venobox preview-link">
+                         <img src="assets/img/abls/homepage.jpg" class="img-fluid" alt="">
+                     </a>
+                 </div>
+
+                 <div class="col-lg-4 col-md-6 portfolio-item filter">
+                     <a href="assets/img/abls/secondpage.jpg" data-gall="portfolioGallery" class="venobox preview-link">
+                         <img src="assets/img/abls/secondpage.jpg" class="img-fluid" alt="">
+                     </a>
+                 </div>
+
+                 <div class="col-lg-4 col-md-6 portfolio-item filter">
+                     <a href="assets/img/abls/thirdpage.jpg" data-gall="portfolioGallery" class="venobox preview-link">
+                         <img src="assets/img/abls/thirdpage.jpg" class="img-fluid" alt="">
+                     </a>
+                 </div>
+
+                 <div class="col-lg-4 col-md-6 portfolio-item filter">
+                     <a href="assets/img/abls/fourthpage.jpg" data-gall="portfolioGallery" class="venobox preview-link">
+                         <img src="assets/img/abls/fourthpage.jpg" class="img-fluid" alt="">
+                     </a>
+                 </div>
+
+                 <div class="col-lg-4 col-md-6 portfolio-item filter">
+                     <a href="assets/img/abls/fifthpage.jpg" data-gall="portfolioGallery" class="venobox preview-link">
+                         <img src="assets/img/abls/fifthpage.jpg" class="img-fluid" alt="">
+                     </a>
+                 </div>
+
+             </div>
+
+     </section>
+
+     <section id="portfolio" class="portfolio">
+         <div class="container">
+             <div class="section-title mt-3" data-aos="fade-up">
+                 <h2><strong>Social Media</strong></h2>
+             </div>
+
+             <div class="row d-flex justify-content-center mx-md-n5">
+                 <a href="https://www.instagram.com/nblc.indonesia/" class="instagram px-3" title="Instagram"><i
+                         class="icofont-instagram icofont-2x"></i></a>
+             </div>
+         </div>
+     </section>
+ </main>
