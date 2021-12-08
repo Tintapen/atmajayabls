7c3b587d9515a799e2232618ef86a17fee9007ab
Tintapen <okiper12@gmail.com>
2021-12-08 08:48:35 +0700

    Repair image event project nblc social

--- application/views/project/v_project.php
+++ application/views/project/v_project.php
@@ -1,62 +1,60 @@
 <main id="main">
-    <section id="breadcrumbs" class="breadcrumbs">
-        <div class="container">
-            <div class="d-flex justify-content-between align-items-center">
-                <h2>Project & Events</h2>
-            </div>
-        </div>
-    </section>
+	<section id="breadcrumbs" class="breadcrumbs">
+		<div class="container">
+			<div class="d-flex justify-content-between align-items-center">
+				<h2>Project & Events</h2>
+			</div>
+		</div>
+	</section>
 
-    <section id="portfolio-details" class="portfolio-details">
-        <div class="container" data-aos="fade-up">
-            <div class="section-title mt-5" data-aos="fade-up">
-                <h2>Project & <strong>Events</strong></h2>
-            </div>
+	<section id="portfolio-details" class="portfolio-details">
+		<div class="container" data-aos="fade-up">
+			<div class="section-title mt-5" data-aos="fade-up">
+				<h2>Project & <strong>Events</strong></h2>
+			</div>
 
-            <div class="row">
-                <div class="col text-center">
-                    <a href="<?= base_url('nblcsummit') ?>">
-                        <div class="h-75 mb-auto">
-                            <img src="assets/img/abls/nblc/NBLC_SUMMIT2.png" class="img-fluid" alt="NBLC Summit">
-                        </div>
-                        <p>NBLC Summit</p>
-                    </a>
-                </div>
-                <div class="col text-center">
-                    <a href="<?= base_url('cla') ?>">
-                        <div class="h-75 mb-auto">
-                            <img src="assets/img/abls/Photo-1.png" class="img-fluid" alt="Corporate Legal Academy">
-                        </div>
-                        <p>Corporate Legal Academy</p>
-                    </a>
-                </div>
-                <div class="col text-center">
-                    <a href="<?= base_url('iloc') ?>">
-                        <div class="h-75 mb-auto">
-                            <img src="assets/img/abls/3.png" class="img-fluid" alt="Internal Legal Opinion Competition">
-                        </div>
-                        <p>Internal Legal Opinion Competition</p>
-                    </a>
-                </div>
-                <div class="col text-center">
-                    <a href="<?= base_url('ablc') ?>">
-                        <div class="h-75 mb-auto">
-                            <img src="assets/img/abls/ABLC.png" class="img-fluid"
-                                alt="Atma Jaya Business Law Competition">
-                        </div>
-                        <p>Atma Jaya Business Law Competition</p>
-                    </a>
-                </div>
-                <div class="col text-center">
-                    <a href="<?= base_url('social') ?>">
-                        <div class="h-75 mb-auto">
-                            <img src="assets/img/abls/nblc.png" class="img-fluid"
-                                alt="NBLC Social Responsibility Webinar">
-                        </div>
-                        <p>NBLC Social Responsibility Webinar</p>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </section>
+			<div class="row">
+				<div class="col text-center">
+					<a href="<?= base_url('nblcsummit') ?>">
+						<div class="h-75 mb-auto">
+							<img src="assets/img/abls/nblc/NBLC_SUMMIT2.png" class="img-fluid" alt="NBLC Summit">
+						</div>
+						<p>NBLC Summit</p>
+					</a>
+				</div>
+				<div class="col text-center">
+					<a href="<?= base_url('cla') ?>">
+						<div class="h-75 mb-auto">
+							<img src="assets/img/abls/Photo-1.png" class="img-fluid" alt="Corporate Legal Academy">
+						</div>
+						<p>Corporate Legal Academy</p>
+					</a>
+				</div>
+				<div class="col text-center">
+					<a href="<?= base_url('iloc') ?>">
+						<div class="h-75 mb-auto">
+							<img src="assets/img/abls/3.png" class="img-fluid" alt="Internal Legal Opinion Competition">
+						</div>
+						<p>Internal Legal Opinion Competition</p>
+					</a>
+				</div>
+				<div class="col text-center">
+					<a href="<?= base_url('ablc') ?>">
+						<div class="h-75 mb-auto">
+							<img src="assets/img/abls/ABLC.png" class="img-fluid" alt="Atma Jaya Business Law Competition">
+						</div>
+						<p>Atma Jaya Business Law Competition</p>
+					</a>
+				</div>
+				<div class="col text-center">
+					<a href="<?= base_url('social') ?>">
+						<div class="h-75 mb-auto">
+							<img src="assets/img/abls/NBLC.png" class="img-fluid" alt="NBLC Social Responsibility Webinar">
+						</div>
+						<p>NBLC Social Responsibility Webinar</p>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
 </main>
