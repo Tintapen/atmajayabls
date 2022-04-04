9a92a9d37de92c249b35c4f964d907fb3d7810df
Tintapen <okiper12@gmail.com>
2022-04-04 11:59:06 +0700

    Remove nblc image

--- application/views/home/v_home.php
+++ application/views/home/v_home.php
@@ -1,106 +1,106 @@
 <!-- ======= Hero Section ======= -->
 <section id="hero">
-    <div id="heroCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+	<div id="heroCarousel" class="carousel slide carousel-fade" data-ride="carousel">
 
-        <div class="carousel-inner" role="listbox">
-            <!-- Slide 1 -->
-            <div class="carousel-item active" style="background-image: url(assets/img/abls/FULL.png);">
+		<div class="carousel-inner" role="listbox">
+			<!-- Slide 1 -->
+			<div class="carousel-item active" style="background-image: url(assets/img/abls/FULL.png);">
 			</div>
-        </div>
+		</div>
 
-      
-    </div>
+
+	</div>
 </section><!-- End Hero -->
 
 <main id="main">
-    <section id="about-us" class="about-us section-bg">
-        <div class="container" data-aos="fade-up">
-            <div class="section-title">
-                <h2>About Us</strong></h2>
-            </div>
+	<section id="about-us" class="about-us section-bg">
+		<div class="container" data-aos="fade-up">
+			<div class="section-title">
+				<h2>About Us</strong></h2>
+			</div>
 
-            <div class="row content text-justify">
-                <div class="col-lg-6" data-aos="fade-right">
-                    <h2>Atma Jaya BLS</h2>
-                    <p>Atma Jaya Business Law Society (“ABLS”) is a business law-based organization
-                        that has a strong vision to make Atma Jaya Business Law Society a pioneer for all
-                        business law organizations in the Faculty of Law throughout Indonesia.</p>
-                    <p>The mission is to personify the vision by:</p>
-                    <ul>
-                        <li><i class="ri-check-double-line"></i> Produce a leading work programs that related to
-                            Business Law.</li>
-                        <li><i class="ri-check-double-line"></i> Provide ABLS active members to become a great human
-                            resources who are
-                            able to compete competitively in the world of Business Law.</li>
-                    </ul>
-                </div>
-                <div class="col-lg-6 pt-4 pt-lg-0" data-aos="fade-left">
-                    <p>
-                        ABLS is a business law society in Atma Jaya Catholic University of Indonesia.
-                        We are the only organization in Atma Jaya Catholic University of Indonesia that
-                        provide and train college students in Economics and Business Law. Why business law?
-                        Indonesia is facing Asean Economic Community, AFTA, and etc. Because of these,
-                        we are here to prepare college students to be professional person,
-                        especially to be a great corporate lawyer.
-                    </p>
-                    <p>
-                        We develop college students’ skill through training, workshop, seminar, and
-                        also business law competition. Our goal is to prepare college students
-                        to be an intelligent person that is useful to face real-world in law firm.
-                    </p>
-                    <p>
-                        ABLS formed in 2014 by the founders. They are Albertus Andhika, Andonius
-                        Simanjorang, Angela Yogiana, Arvin Raharja, Peter Pendowo, Selfi, and Yoanna
-                        Syvia Dessyadventia Ben.
-                    </p>
-                </div>
-            </div>
-        </div>
-    </section>
+			<div class="row content text-justify">
+				<div class="col-lg-6" data-aos="fade-right">
+					<h2>Atma Jaya BLS</h2>
+					<p>Atma Jaya Business Law Society (“ABLS”) is a business law-based organization
+						that has a strong vision to make Atma Jaya Business Law Society a pioneer for all
+						business law organizations in the Faculty of Law throughout Indonesia.</p>
+					<p>The mission is to personify the vision by:</p>
+					<ul>
+						<li><i class="ri-check-double-line"></i> Produce a leading work programs that related to
+							Business Law.</li>
+						<li><i class="ri-check-double-line"></i> Provide ABLS active members to become a great human
+							resources who are
+							able to compete competitively in the world of Business Law.</li>
+					</ul>
+				</div>
+				<div class="col-lg-6 pt-4 pt-lg-0" data-aos="fade-left">
+					<p>
+						ABLS is a business law society in Atma Jaya Catholic University of Indonesia.
+						We are the only organization in Atma Jaya Catholic University of Indonesia that
+						provide and train college students in Economics and Business Law. Why business law?
+						Indonesia is facing Asean Economic Community, AFTA, and etc. Because of these,
+						we are here to prepare college students to be professional person,
+						especially to be a great corporate lawyer.
+					</p>
+					<p>
+						We develop college students’ skill through training, workshop, seminar, and
+						also business law competition. Our goal is to prepare college students
+						to be an intelligent person that is useful to face real-world in law firm.
+					</p>
+					<p>
+						ABLS formed in 2014 by the founders. They are Albertus Andhika, Andonius
+						Simanjorang, Angela Yogiana, Arvin Raharja, Peter Pendowo, Selfi, and Yoanna
+						Syvia Dessyadventia Ben.
+					</p>
+				</div>
+			</div>
+		</div>
+	</section>
 
-    <section id="clients" class="clients">
-        <div class="container" data-aos="fade-up">
-            <div class="section-title">
-                <h2>Project & Events</h2>
-            </div>
+	<section id="clients" class="clients">
+		<div class="container" data-aos="fade-up">
+			<div class="section-title">
+				<h2>Project & Events</h2>
+			</div>
 
-            <div class="row no-gutters clients-wrap clearfix" data-aos="fade-up">
-                <div class="col-lg-3 col-md-4 col-6">
+			<div class="row no-gutters clients-wrap clearfix" data-aos="fade-up">
+				<div class="col-lg-3 col-md-4 col-6">
+					<div class="client-logo">
+						<a href="<?= base_url('nblcsummit') ?>">
+							<img src="assets/img/abls/nblc/NBLC_SUMMIT2.png" class="img-fluid" alt="">
+						</a>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-4 col-6">
+					<div class="client-logo">
+						<a href="<?= base_url('cla') ?>">
+							<img src="assets/img/abls/Photo-1.png" class="img-fluid" alt="">
+						</a>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-4 col-6">
+					<div class="client-logo">
+						<a href="<?= base_url('iloc') ?>">
+							<img src="assets/img/abls/3.png" class="img-fluid" alt="" style="width:75px;height:70px;">
+						</a>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-4 col-6">
+					<div class="client-logo">
+						<a href="<?= base_url('ablc') ?>">
+							<img src="assets/img/abls/ABLC.png" class="img-fluid" alt="">
+						</a>
+					</div>
+				</div>
+				<!-- <div class="col-lg-3 col-md-4 col-6">
                     <div class="client-logo">
-                        <a href="<?= base_url('nblcsummit') ?>">
-                            <img src="assets/img/abls/nblc/NBLC_SUMMIT2.png" class="img-fluid" alt="">
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4 col-6">
-                    <div class="client-logo">
-                        <a href="<?= base_url('cla') ?>">
-                            <img src="assets/img/abls/Photo-1.png" class="img-fluid" alt="">
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4 col-6">
-                    <div class="client-logo">
-                        <a href="<?= base_url('iloc') ?>">
-                            <img src="assets/img/abls/3.png" class="img-fluid" alt="" style="width:75px;height:70px;">
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4 col-6">
-                    <div class="client-logo">
-                        <a href="<?= base_url('ablc') ?>">
-                            <img src="assets/img/abls/ABLC.png" class="img-fluid" alt="">
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4 col-6">
-                    <div class="client-logo">
-                        <a href="<?= base_url('social') ?>">
+                        <a href="<? //= base_url('social') 
+									?>">
                             <img src="assets/img/abls/nblc.png" class="img-fluid" alt="">
                         </a>
                     </div>
-                </div>
-            </div>
-    </section>
+                </div> -->
+			</div>
+	</section>
 </main>
-
