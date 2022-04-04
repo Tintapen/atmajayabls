9f739db1a1bd8d6ea629176ce1ab68545fb778ac
Tintapen <okiper12@gmail.com>
2022-04-04 11:56:24 +0700

    Add meta

--- application/views/_partials/head.php
+++ application/views/_partials/head.php
@@ -2,8 +2,8 @@
 <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
 <title>ABLS - ATMAJAYA</title>
-<meta content="" name="description">
-<meta content="" name="keywords">
+<meta content="atmajaya abls" name="description">
+<meta content="cla" name="keywords">
 
 <!-- Favicons -->
 <link href="<?= base_url('assets/img/abls/logo_new.png') ?>" rel="icon">
